b1f75db88ce006999763fbd4d8a0bffbdb9f6c31
sachin-5598 <ext-sachin.baiswar@unacademy.com>
2020-11-09 11:12:19 +0530

    :v: added schema validation for signup

--- server/auth/requestHandlers/signupHandler.js
+++ server/auth/requestHandlers/signupHandler.js
@@ -1,8 +1,27 @@
+const Joi = require('joi');
+
+const schema = Joi.object({
+  username: Joi.string()
+      .trim()
+      .pattern(new RegExp('^[a-zA-Z0-9_]+$'))
+      .min(2)
+      .max(30)
+      .required(),
+
+  password: Joi.string()
+      .trim()
+      .pattern(new RegExp('^[a-zA-Z0-9@#$%^&*]{8,30}$')),
+});
+
 const signupHandler = (req, res, next) => {
-  console.log(req.body);
-  res.json({
-    message: 'signup in progress'
-  });
+  const {error, value} = schema.validate(req.body);
+  if (error) {
+    res.status(422);
+    next(new Error(error));
+  } else {
+    // insert in db
+    res.json(value);
+  }
 }
 
 module.exports = signupHandler;
